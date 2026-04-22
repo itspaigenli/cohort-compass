143822846ff8b150797bbc8c364e363fbb55137a
Paige Li <itspaige.nli@gmail.com>
2026-04-21 21:18:13 -0700

    Finish setup on test

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,1 +1,14 @@
 import "@testing-library/jest-dom";
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    addListener: () => {},
+    removeListener: () => {},
+    dispatchEvent: () => false,
+  }),
+});
