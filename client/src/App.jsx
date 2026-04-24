cc348a995dad932da95b36db15b4db0fb4606d48
Paige Li <itspaige.nli@gmail.com>
2026-04-24 14:19:21 -0700

    Update App.jsx to render new react component

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import ScheduleList from "./components/ScheduleList.jsx";
+
+export default function App() {
+  return (
+    <main>
+      <h1>Cohort Compass</h1>
+      <ScheduleList />
+    </main>
+  );
+}
