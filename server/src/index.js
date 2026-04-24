0568f1dca03765bc8222bf560f8c6caac4b96bea
Paige Li <itspaige.nli@gmail.com>
2026-04-24 12:59:28 -0700

    Mount router for schedule items

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { checkDatabaseConnection } from "./config/db.js";
+import scheduleRoutes from "./routes/scheduleRoutes.js";
 
 dotenv.config();
 
@@ -14,6 +15,8 @@ app.use(
 );
 
 app.use(express.json());
+
+app.use("/api/schedule", scheduleRoutes);
 
 app.get("/", async (req, res) => {
   const databaseConnected = await checkDatabaseConnection();
