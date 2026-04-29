7886d9701aa81ed409489543b8942401000848f4
Paige Li <itspaige.nli@gmail.com>
2026-04-29 16:55:50 -0700

    Add FAQ model

---
+++ server/src/models/faqModel.js
@@ -0,0 +1,26 @@
+import { query } from "../config/db.js";
+
+export async function listFaqEntries() {
+  const result = await query(
+    `SELECT
+      faq_entries.id,
+      faq_entries.question,
+      faq_entries.answer,
+      faq_entries.error_topic,
+      faq_entries.category,
+      COALESCE(
+        array_agg(tags.name ORDER BY tags.name)
+          FILTER (WHERE tags.name IS NOT NULL),
+        ARRAY[]::varchar[]
+      ) AS tags
+    FROM faq_entries
+    LEFT JOIN faq_tags
+      ON faq_tags.faq_entry_id = faq_entries.id
+    LEFT JOIN tags
+      ON tags.id = faq_tags.tag_id
+    GROUP BY faq_entries.id
+    ORDER BY faq_entries.category ASC, faq_entries.question ASC`,
+  );
+
+  return result.rows;
+}
