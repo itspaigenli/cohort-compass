5a735c786bec9e8c9faf827a0328bb0ddc921c73
Paige Li <itspaige.nli@gmail.com>
2026-05-11 21:06:43 -0700

    Create search page shell

---
+++ client/src/pages/SearchPage.jsx
@@ -0,0 +1,8 @@
+export default function SearchPage() {
+  return (
+    <section className="search-page" aria-labelledby="search-page-heading">
+      <p className="eyebrow">Student search</p>
+      <h1 id="search-page-heading">Search the Student Hub</h1>
+    </section>
+  );
+}
