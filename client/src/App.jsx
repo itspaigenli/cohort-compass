028db61393ce14611273d2e029186c0ef99e0126
Paige Li <itspaige.nli@gmail.com>
2026-05-11 21:14:37 -0700

    Render search page from hash

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import DashboardPage from "./pages/DashboardPage.jsx";
+import SearchPage from "./pages/SearchPage.jsx";
 
 function App() {
+  const page = window.location.hash === "#search" ? "search" : "dashboard";
+
   return (
     <main className="app-shell">
-      <DashboardPage />
+      {page === "search" ? <SearchPage /> : <DashboardPage />}
     </main>
   );
 }
