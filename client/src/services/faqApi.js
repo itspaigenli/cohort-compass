bda1b97f4a1444fdb2360037deeabf21049cd239
Paige Li <itspaige.nli@gmail.com>
2026-05-18 20:08:36 -0700

    Add FAQ API fallback

--- client/src/services/faqApi.js
+++ client/src/services/faqApi.js
@@ -1,7 +1,8 @@
 import { getJson } from "./api.js";
+import { mockFaqEntries } from "../data/mockData.js";
 
 export async function fetchFaqEntries() {
-  const data = await getJson("/faq");
+  const data = await getJson("/faq", { faqEntries: mockFaqEntries });
 
   return data.faqEntries;
 }
