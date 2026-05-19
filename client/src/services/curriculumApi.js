9946f64e7aac383598141f5088350400bdfc1bff
Paige Li <itspaige.nli@gmail.com>
2026-05-18 20:09:47 -0700

    Add curriculum API fallback

--- client/src/services/curriculumApi.js
+++ client/src/services/curriculumApi.js
@@ -1,7 +1,10 @@
 import { getJson } from "./api.js";
+import { mockCurriculumReferences } from "../data/mockData.js";
 
 export async function fetchCurriculumReferences() {
-  const data = await getJson("/curriculum");
+  const data = await getJson("/curriculum", {
+    curriculumReferences: mockCurriculumReferences,
+  });
 
   return data.curriculumReferences || [];
 }
